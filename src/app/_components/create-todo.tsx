07f59b8b9553247f30c1c169d6ca8a9015e745f0
MuhammadUsamaAwan <underusama@gmail.com>
2024-03-13 09:44:09 +0500

    useTransition -> useFormStatus

--- src/app/_components/create-todo.tsx
+++ src/app/_components/create-todo.tsx
@@ -1,19 +1,34 @@
 'use client';
 
-import { useTransition } from 'react';
+// import { useTransition } from 'react';
+import { useFormStatus } from 'react-dom';
 
 import { addTodo } from '~/lib/actions';
 import { Button } from '~/components/ui/button';
 
-export default function CreateTodo() {
-  const [pending, startTransition] = useTransition();
+// export default function CreateTodo() {
+//   const [pending, startTransition] = useTransition();
 
+//   return (
+//     <Button
+//       disabled={pending}
+//       onClick={() => startTransition(() => addTodo().catch(() => alert('Failed to create todo')))}
+//     >
+//       Add New
+//     </Button>
+//   );
+// }
+
+export default function CreateTodo() {
   return (
-    <Button
-      disabled={pending}
-      onClick={() => startTransition(() => addTodo().catch(() => alert('Failed to create todo')))}
-    >
-      Add New
-    </Button>
+    <form action={addTodo}>
+      <SubmitButton />
+    </form>
   );
+}
+
+function SubmitButton() {
+  const { pending } = useFormStatus();
+
+  return <Button disabled={pending}>Add New</Button>;
 }
